8c5fc4d83d717743690ebc7bf5d3e0232519d69a
Chris Hatch <tofu@redlen.tl>
2016-03-16 17:40:01 +0700

    divide pm2.5 and pm10 by 10 to get the correct value take a read once every 30 seconds for now (possibly change later to an average of 30 reads over 30 seconds)

--- NovaSDS011/NovaSDS011.ino
+++ NovaSDS011/NovaSDS011.ino
@@ -6,22 +6,19 @@
  */
 
 /*
- * Spec here http://inovafitness.com/upload/file/20150311/14261262164716.pdf
+ * PM Sensor spec here http://inovafitness.com/upload/file/20150311/14261262164716.pdf
  * 
+ * packet format: AA C0 pm2_5_Low pm2_5_High pm10_Low pm10_High 0 0 CRC AB
  * Check-sum: DATA1+DATA2+...+DATA6
  * PM2.5 value: PM2.5(ug/m3) = ((PM2.5 highbyte * 256) + PM2.5 lowbyte) / 10
- * PM10 value: PM10(ug/m3) = ((PM10 highbyte * 256) + PM10 lowbyte) / 10
+ * pm10 value: pm10(ug/m3) = ((pm10 highbyte * 256) + pm10 lowbyte) / 10
  */
 #include <SoftwareSerial.h>
 
-SoftwareSerial pmSerial(8, 9, false);
-
-// this is a demo for Arduino PM2.5 sensor test
-// PM2.5 sensor is from www.inovafitness.com SDS011
-unsigned int Pm25 = 0;//used for result pm2.5
-unsigned int Pm10 = 0;//used for result pm10
-unsigned char Pm25IsNew = 0;//show if pm25 is refreshed
-
+// use software serial to capture sensor data
+const int RX_PIN = 8; 
+const int TX_PIN = 9; 
+SoftwareSerial pmSerial(RX_PIN, TX_PIN, false);
 
 void ProcessSerialData()
 {
@@ -31,8 +28,7 @@ void ProcessSerialData()
   uint8_t mCheck = 0;
  while (pmSerial.available() > 0) 
   {  
-    // from www.inovafitness.com
-    // packet format: AA C0 PM25_Low PM25_High PM10_Low PM10_High 0 0 CRC AB
+    // packet format: AA C0 pm2_5_Low pm2_5_High pm10_Low pm10_High 0 0 CRC AB
      mData = pmSerial.read();     delay(2);
     if(mData == 0xAA)//head1 ok
      {
@@ -55,24 +51,22 @@ void ProcessSerialData()
           mPkt[9] = pmSerial.read();
           if(mCheck == mPkt[8]) //crc ok
           {
-            Serial.print("checksum=");
-            Serial.println(mCheck);
             pmSerial.flush();
-            //pmSerial.write(mPkt,10);
 
             // PM2.5 value: PM2.5(ug/m3) = ((PM2.5 highbyte * 256) + PM2.5 lowbyte) / 10
-            // PM10 value: PM10(ug/m3) = ((PM10 highbyte * 256) + PM10 lowbyte) / 10
-            Pm25 = (uint16_t)mPkt[2] | (uint16_t)(mPkt[3]<<8);
-            Pm10 = (uint16_t)mPkt[4] | (uint16_t)(mPkt[5]<<8);
-            if(Pm25 > 9999)
-             Pm25 = 9999;
-            if(Pm10 > 9999)
-             Pm10 = 9999;            
-            //get one good packet
-            Pm25IsNew = 1;
-            Serial.print(Pm25);
+            // pm10 value: pm10(ug/m3) = ((pm10 highbyte * 256) + pm10 lowbyte) / 10
+            unsigned int pm2_5 = (uint16_t)mPkt[2] | (uint16_t)(mPkt[3]<<8);
+            unsigned int pm10 = (uint16_t)mPkt[4] | (uint16_t)(mPkt[5]<<8);
+            if(pm2_5 > 9999)
+             pm2_5 = 9999;
+            if(pm10 > 9999)
+             pm10 = 9999;        
+            
+            float finalPm2_5 = pm2_5 / 10;
+            float finalPm10 = pm10 / 10;
+            Serial.print(finalPm2_5);
             Serial.print(",");
-            Serial.println(Pm10);
+            Serial.println(finalPm10);
              return;
           }
         }      
@@ -89,5 +83,6 @@ void setup() {
 
 void loop() {
   ProcessSerialData();
+  delay(29000);
 }
 
